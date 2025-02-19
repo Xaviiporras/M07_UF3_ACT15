9e01082013e9e31b283a0642904463bb2cb84385
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2025-02-19 20:28:24 +0100

    Preparació model.py cart i usuari

--- botiga/cart/models.py
+++ botiga/cart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
