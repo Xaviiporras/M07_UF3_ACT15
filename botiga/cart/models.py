be89b5d5921e18524384df7c3913f21702a7aab6
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2025-02-26 19:43:48 +0100

    completar model cart

--- botiga/cart/models.py
+++ botiga/cart/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+
+#Modelo de Usuario
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
 
+#Modelo de Carrito
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#Modelo de Item de Carrito
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product_id = models.IntegerField()  # Relació amb producte (de moment nomes el id ja que no tinc la part del Xavi encara, encara no hem fet merge)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x Producto {self.product_id} en carrito {self.cart.id}"
