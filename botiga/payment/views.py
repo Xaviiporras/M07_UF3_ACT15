0476cd1757b97ff024fe5ede6a0c7c1360d107c2
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2025-03-01 17:02:53 +0100

    logica verificacio dades de pagament

--- botiga/payment/views.py
+++ botiga/payment/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Payment
 
-# Create your views here.
+@api_view(['GET'])
+def get_payments(request):
+    payments = Payment.objects.all().values()
+    return Response(list(payments), status=status.HTTP_200_OK)
