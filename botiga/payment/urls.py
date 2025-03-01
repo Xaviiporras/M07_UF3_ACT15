ad42e7c5261f6f6582e7119fdff73f7924a38b37
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2025-03-01 18:09:31 +0100

    actualizar campo status de tabla order

--- botiga/payment/urls.py
+++ botiga/payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_payments, get_user_ids
+from .views import get_payments, get_user_ids, update_order_status
 
 urlpatterns = [
     path('payments/', get_payments, name='get_payments'),
     path('users/ids/', get_user_ids, name='get_user_ids'),
+    path('orders/<int:order_id>/update_status/', update_order_status, name='update_order_status'),
 ]
--- botiga/payment/views.py
+++ botiga/payment/views.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Payment
-from botiga.cart import user
+from cart.models import user
+from orders.models import Order
 
 @api_view(['GET'])
 def get_payments(request):
@@ -13,3 +14,20 @@ def get_payments(request):
 def get_user_ids(request):
     user_ids = user.objects.values_list('id', flat=True)
     return Response(list(user_ids), status=status.HTTP_200_OK)
+
+@api_view(['PUT'])
+def update_order_status(request, order_id):
+    try:
+        order = Order.objects.get(id=order_id)
+    except Order.DoesNotExist:
+        return Response({"error": "Orden no encontrada"}, status=status.HTTP_404_NOT_FOUND)
+
+    new_status = request.data.get('status')
+
+    if not new_status or not isinstance(new_status, str):
+        return Response({"error": "El estado no puede estar vacío y debe ser un texto válido."}, status=status.HTTP_400_BAD_REQUEST)
+
+    order.status = new_status
+    order.save()
+
+    return Response({"message": "Estado actualizado correctamente", "order_id": order.id, "new_status": order.status}, status=status.HTTP_200_OK)
